a35980233a69d75b95bd052cf6f6b208e06f55cc
Kasumi Hanazuki <kasumi@rollingapple.net>
2015-06-11 19:37:01 +0900

    Documentation!

--- lib/fibonacci.ex
+++ lib/fibonacci.ex
@@ -1,4 +1,8 @@
 defmodule Fibonacci do
+  @moduledoc """
+  This is the Fibonacci module!
+  """
+
   use Application
 
   def start(_type, _args) do
@@ -14,6 +18,15 @@ defmodule Fibonacci do
     Supervisor.start_link(children, opts)
   end
 
+  @doc """
+  Returns the next fibonacci number.
+
+      iex> Fibonacci.next
+      0
+      iex> Fibonacci.next
+      1
+  """
   def next, do: Fibonacci.Server.next
+
   def crash!, do: Fibonacci.Server.crash!
 end
