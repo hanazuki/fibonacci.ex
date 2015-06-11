45f8c48bf6958be03d5065221f1cab84935e7036
Kasumi Hanazuki <kasumi@rollingapple.net>
2015-06-11 19:35:35 +0900

    Write simple test

--- test/fibonacci_test.exs
+++ test/fibonacci_test.exs
@@ -1,7 +1,13 @@
 defmodule FibonacciTest do
   use ExUnit.Case
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "fibonacci" do
+    assert Fibonacci.next == 0
+    assert Fibonacci.next == 1
+    assert Fibonacci.next == 1
+    assert Fibonacci.next == 2
+    assert Fibonacci.next == 3
+    assert Fibonacci.next == 5
+    assert Fibonacci.next == 8
   end
 end
